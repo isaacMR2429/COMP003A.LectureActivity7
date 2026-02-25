b47906d55808e0b57bf0d85693aaa6dd1bf067cf
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-02-25 15:44:43 -0800

    Add project files.

---
+++ COMP003A.LectureActivity7/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.LectureActivity7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
