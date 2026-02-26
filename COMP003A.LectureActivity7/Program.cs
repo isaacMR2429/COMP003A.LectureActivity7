172cbd704fa268e9f40114918994f45704a8d221
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-02-25 17:09:35 -0800

    validated the input (logic only) using if/else

--- COMP003A.LectureActivity7/Program.cs
+++ COMP003A.LectureActivity7/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace COMP003A.LectureActivity7
+﻿using System.ComponentModel.Design;
+
+namespace COMP003A.LectureActivity7
 {
     internal class Program
     {
@@ -15,10 +17,28 @@
             names.Add("Alex");
             names.Add("Jordan");
             names.Add("Taylor");
-            Console.WriteLine("\nNames:");
+            Console.WriteLine("\nNames (using foreach):");
             foreach (string name in names)
             {
                 Console.WriteLine(name);
+            }
+
+            Console.WriteLine("\nNames (using for): ");
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            Console.Write("\nEnter a number between 1 and 3: ");
+            string input = Console.ReadLine();
+            bool theNumber = int.TryParse(input, out int validatedNumber);
+            if (theNumber && validatedNumber >= 1 && validatedNumber <= 3)
+            {
+                Console.WriteLine("Valid number");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number, please try again and pick between 1 and 3");
             }
         }
     }
